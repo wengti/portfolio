f4eea352441f1bb72eaabf2dee2dd228f3bd204f
Weng Ti <wengti@hotmail.com>
2026-04-08 18:02:21 +0800

    Complete contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/contact/ContactForm";
+
+export default function ContactPage() {
+
+    return (
+        <div className='grow flex flex-col justify-center'>
+            <div className=''>
+                <ContactForm />
+            </div>
+        </div>
+    )
+}
