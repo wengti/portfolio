4fb5213ec0c9c6f2054a7ae97de4dcf4fe304c97
Weng Ti <wengti@hotmail.com>
2026-04-09 17:07:27 +0800

    add link to project card

--- components/home/HomeProjectsGrid.tsx
+++ components/home/HomeProjectsGrid.tsx
@@ -1,6 +1,7 @@
 import { ProjectsDataType } from '@/type/type'
 import { motion } from 'motion/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 
@@ -17,48 +18,53 @@ export default function HomeProjectsGrid({ projectsData, isLastItemHidden }: { p
                     const extraClsName = (idx === projectsData.length - 1 && isLastItemHidden) ? 'hidden sm:flex lg:hidden' : ''
 
                     return (
-                        <motion.div
+                        <Link
+                            href={`/projects/${data.filename}`}
+                            className='hover:scale-103 active:scale-103 flex'
                             key={data.name}
-                            className={`flex flex-col gap-2 bg-card-black py-2 border-2 border-edge-yellow rounded-2xl ${extraClsName}`}
-                            initial={{ opacity: 0, y: 25 }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 0.25 * idx }}
-                            viewport={{ once: true }}
                         >
+                            <motion.div
+                                className={`flex flex-col gap-2 bg-card-black py-2 border-2 border-edge-yellow rounded-2xl ${extraClsName}`}
+                                initial={{ opacity: 0, y: 25 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1, delay: 0.25 * idx }}
+                                viewport={{ once: true }}
+                            >
 
-                            {/* Cover Image for each project */}
-                            <div>
-                                <Image
-                                    src={`/${data.filename}/1.png`}
-                                    alt={`The feature image for the project ${data.name}.`}
-                                    height={1080}
-                                    width={1920}
-                                    className="rounded-2xl w-14/15 mx-auto"
-                                />
-                            </div>
-
-                            {/* Description of the project */}
-                            <div className='px-4 text-xs flex flex-col gap-1'>
-                                <p>
-                                    {data.type.toUpperCase()} | {data.year}
-                                </p>
-                                <h3 className='font-bold text-letter-white text-2xl'>
-                                    {data.name}
-                                </h3>
-                                <div className='flex gap-1 flex-wrap'>
-                                    {
-                                        data.tech.map(item =>
-                                            <div className='bg-card-yellow text-letter-yellow border border-edge-yellow rounded-sm w-fit px-2 py-1' key={item}>
-                                                {item}
-                                            </div>
-                                        )
-                                    }
+                                {/* Cover Image for each project */}
+                                <div>
+                                    <Image
+                                        src={`/${data.filename}/1.png`}
+                                        alt={`The feature image for the project ${data.name}.`}
+                                        height={1080}
+                                        width={1920}
+                                        className="rounded-2xl w-14/15 mx-auto"
+                                    />
                                 </div>
-                                <p className='leading-6 text-base mt-2 text-letter-mute2'>
-                                    {data.description}
-                                </p>
-                            </div>
-                        </motion.div>
+
+                                {/* Description of the project */}
+                                <div className='px-4 text-xs flex flex-col gap-1'>
+                                    <p>
+                                        {data.type.toUpperCase()} | {data.year}
+                                    </p>
+                                    <h3 className='font-bold text-letter-white text-2xl'>
+                                        {data.name}
+                                    </h3>
+                                    <div className='flex gap-1 flex-wrap'>
+                                        {
+                                            data.tech.map(item =>
+                                                <div className='bg-card-yellow text-letter-yellow border border-edge-yellow rounded-sm w-fit px-2 py-1' key={item}>
+                                                    {item}
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                    <p className='leading-6 text-base mt-2 text-letter-mute2'>
+                                        {data.description}
+                                    </p>
+                                </div>
+                            </motion.div>
+                        </Link>
 
                     )
                 })
--- components/projects/ProjectsSingleCard.tsx
+++ components/projects/ProjectsSingleCard.tsx
@@ -1,55 +1,61 @@
 import { ProjectsDataType } from '@/type/type'
 import { motion } from 'motion/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function ProjectsSingleCard({data, isHiddenOnLargeScreen}: {data: ProjectsDataType, isHiddenOnLargeScreen: boolean}) {
+export default function ProjectsSingleCard({ data, isHiddenOnLargeScreen }: { data: ProjectsDataType, isHiddenOnLargeScreen: boolean }) {
 
 
     const hiddenCls = isHiddenOnLargeScreen ? 'lg:hidden' : ''
 
     return (
-        <motion.div
+        <Link
+            href={`/projects/${data.filename}`}
+            className={`flex hover:scale-103 active:scale-103 ${hiddenCls}`}
             key={data.name}
-            className={`flex flex-col gap-2 bg-card-black py-2 border-2 border-edge-yellow rounded-2xl min-h-125 ${hiddenCls}`}
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.25 }}
-            viewport={{ once: true }}
         >
+            <motion.div
+                className={`flex flex-col gap-2 bg-card-black py-2 border-2 border-edge-yellow rounded-2xl min-h-125`}
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.25 }}
+                viewport={{ once: true }}
+            >
 
-            {/* Cover Image for each project */}
-            <div>
-                <Image
-                    src={`/${data.filename}/1.png`}
-                    alt={`The feature image for the project ${data.name}.`}
-                    height={1080}
-                    width={1920}
-                    className="rounded-2xl w-14/15 mx-auto"
-                />
-            </div>
-
-            {/* Description of the project */}
-            <div className='px-4 text-xs flex flex-col gap-1'>
-                <p>
-                    {data.type.toUpperCase()} | {data.year}
-                </p>
-                <h3 className='font-bold text-letter-white text-2xl'>
-                    {data.name}
-                </h3>
-                <div className='flex gap-1 flex-wrap'>
-                    {
-                        data.tech.map(item =>
-                            <div className='bg-card-yellow text-letter-yellow border border-edge-yellow rounded-sm w-fit px-2 py-1' key={item}>
-                                {item}
-                            </div>
-                        )
-                    }
+                {/* Cover Image for each project */}
+                <div>
+                    <Image
+                        src={`/${data.filename}/1.png`}
+                        alt={`The feature image for the project ${data.name}.`}
+                        height={1080}
+                        width={1920}
+                        className="rounded-2xl w-14/15 mx-auto"
+                    />
                 </div>
-                <p className='leading-6 text-base mt-2 text-letter-mute2'>
-                    {data.description}
-                </p>
-            </div>
-        </motion.div>
+
+                {/* Description of the project */}
+                <div className='px-4 text-xs flex flex-col gap-1'>
+                    <p>
+                        {data.type.toUpperCase()} | {data.year}
+                    </p>
+                    <h3 className='font-bold text-letter-white text-2xl'>
+                        {data.name}
+                    </h3>
+                    <div className='flex gap-1 flex-wrap'>
+                        {
+                            data.tech.map(item =>
+                                <div className='bg-card-yellow text-letter-yellow border border-edge-yellow rounded-sm w-fit px-2 py-1' key={item}>
+                                    {item}
+                                </div>
+                            )
+                        }
+                    </div>
+                    <p className='leading-6 text-base mt-2 text-letter-mute2'>
+                        {data.description}
+                    </p>
+                </div>
+            </motion.div>
+        </Link>
 
     )
 }
