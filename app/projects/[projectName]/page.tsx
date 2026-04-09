76c3a23fae4b6275f9e258691b92b3aa89caaed8
Weng Ti <wengti@hotmail.com>
2026-04-09 16:53:56 +0800

    Added navigation back and forward btn

--- app/projects/[projectName]/page.tsx
+++ app/projects/[projectName]/page.tsx
@@ -5,15 +5,15 @@ import ProjectDetailSummary from "@/components/projectDetail/ProjectDetailSummar
 import ProjectDetailTechCard from "@/components/projectDetail/ProjectDetailTechCard";
 import { projectsData } from "@/data/projects"
 import Link from "next/link"
-import { FaGithub } from "react-icons/fa6";
-import { GrDeploy } from "react-icons/gr";
-import { LuPresentation } from "react-icons/lu";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 export default async function ProjectDetailPage({ params }: { params: Promise<{ projectName: string }> }) {
 
     const { projectName } = await params
     const dataIdx = projectsData.findIndex((data) => data.filename === projectName)
     const data = projectsData[dataIdx]
+    const lastData = dataIdx === 0 ? null : projectsData[dataIdx - 1]
+    const nextData = dataIdx === (projectsData.length - 1) ? null : projectsData[dataIdx + 1]
 
     return (
         <section className='px-6 flex flex-col gap-2'>
@@ -24,12 +24,14 @@ export default async function ProjectDetailPage({ params }: { params: Promise<{ 
             </div>
 
             <div className='pt-6 pb-12 border-t border-edge-gray flex flex-col gap-6'>
+
                 {/* Overview */}
                 <div>
                     <h2 className='text-letter-yellow font-semibold text-sm mb-1'>OVERVIEW</h2>
                     <p className='text-letter-mute2'>{data.overview}</p>
                 </div>
 
+                {/* Tech Stack and Feature */}
                 <div className='flex flex-col gap-4'>
                     <h2 className='text-letter-yellow font-semibold text-sm'>IMPLEMENTATION</h2>
                     <div className='flex flex-col gap-4 md:flex-row md:gap-8 justify-center'>
@@ -38,11 +40,14 @@ export default async function ProjectDetailPage({ params }: { params: Promise<{ 
                     </div>
                 </div>
 
+                {/* Challenges & Solutions */}
                 <div className='flex flex-col gap-4'>
                     <h2 className='text-letter-yellow font-semibold text-sm'>CHALLENGES & SOLUTIONS</h2>
                     <ProjectDetailChallenges data={data} />
                 </div>
 
+
+                {/* Publication */}
                 {
                     data.references &&
                     <div className='flex flex-col gap-4'>
@@ -51,7 +56,7 @@ export default async function ProjectDetailPage({ params }: { params: Promise<{ 
                             {
                                 data.references.map((reference, idx) => {
                                     return (
-                                        <li className='text-letter-mute2'><span className='text-letter-yellow'>{idx+1}.</span> {reference}</li>
+                                        <li className='text-letter-mute2'><span className='text-letter-yellow'>{idx + 1}.</span> {reference}</li>
                                     )
                                 })
                             }
@@ -59,8 +64,41 @@ export default async function ProjectDetailPage({ params }: { params: Promise<{ 
                     </div>
                 }
 
-                <div>
-                    {/* Button to last and next project */}
+                {/* Navigation button */}
+                <div className='flex gap-4 justify-between mt-8'>
+                    {
+                        lastData ? 
+                        <button className='max-w-100 w-1/2'>
+                            <Link 
+                                href={`/projects/${lastData?.filename}`}
+                                className='flex flex-col px-4 py-2 text-left rounded-xl border border-edge-gray hover:bg-edge-yellow active:bg-edge-yellow'
+                            >
+                                <div className='flex gap-2 items-center text-xs tracking-wide font-semibold'>
+                                    <FaArrowLeftLong />
+                                    <p>LAST PROJECT</p>
+                                </div>
+                                <p className="text-letter-white">{lastData.name.length > 23 ? lastData.name.slice(0,20).trim() + '...' : lastData.name}</p>
+                            </Link>
+                        </button>:
+                        <button className='max-w-100 w-1/2'></button>
+                    }
+
+                    {
+                        nextData ?
+                        <button className='max-w-100 w-1/2'>
+                            <Link 
+                                href={`/projects/${nextData?.filename}`}
+                                className='flex flex-col px-4 py-2 text-right rounded-xl border border-edge-gray hover:bg-edge-yellow active:bg-edge-yellow'
+                            >
+                                <div className='flex gap-2 items-center justify-end text-xs tracking-wide font-semibold'>
+                                    <p>NEXT PROJECT</p>
+                                    <FaArrowRightLong />
+                                </div>
+                                <p className="text-letter-white">{nextData.name.length > 23 ? nextData.name.slice(0,20).trim() + '...' : nextData.name}</p>
+                            </Link>
+                        </button> :
+                        <button className='max-w-100 w-1/2'></button>
+                    }
                 </div>
 
             </div>
