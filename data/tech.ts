88f686d89eb635b78e0b7d49e671f4269bcd0b99
Weng Ti <wengti@hotmail.com>
2026-05-17 14:15:59 +0800

    update tech stacks

--- data/tech.ts
+++ data/tech.ts
@@ -1,9 +1,10 @@
 export const techData = {
     frontend: ['HTML', 'JavaScript', 'TypeScript', 'React', 'React Router'],
     styling: ['CSS', 'Tailwind CSS', 'Tailgrids', 'Material UI(MUI)', 'Framer Motion'],
-    backend: ['Express.js', 'PostgreSQL', 'Supabase', 'RESTful API'],
+    backend: ['Express.js', 'FastAPI', 'PostgreSQL', 'RESTful API'],
+    database: ['Supabase', 'Pinecone'],
     fullstack: ['Next.js'],
-    ai: ['Python', 'PyTorch', 'OpenAI API', 'Scikit-learn', 'Retrieval-Augmented Generation (RAG)', 'You Only Look Once (YOLO)'],
-    others: ['Git', 'Assembly', 'Python', 'C', 'Postman', 'Vite', 'Stripe'],
-    highlight: ['Tailwind CSS', 'React', 'React Router', 'PostgreSQL', 'Supabase', 'Next.js', 'Git', 'Python', 'PyTorch', 'OpenAI API', 'Retrieval-Augmented Generation (RAG)', 'You Only Look Once (YOLO)']
+    ai: ['Python', 'PyTorch', 'LangChain', 'LangGraph', 'Gemini API', 'OpenAI API', 'Scikit-learn', 'Retrieval-Augmented Generation (RAG)', 'You Only Look Once (YOLO)'],
+    others: ['Git', 'pytest', 'Assembly', 'Python', 'C', 'Postman', 'Vite', 'Stripe'],
+    highlight: ['Tailwind CSS', 'React', 'React Router', 'PostgreSQL', 'Supabase', 'Next.js', 'Git', 'Python', 'PyTorch', 'OpenAI API', 'Retrieval-Augmented Generation (RAG)', 'You Only Look Once (YOLO)', 'Pinecone', 'LangChain', 'LangGraph', 'pytest']
 }
